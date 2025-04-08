bc61b703709d0d6a3f68e87be919a8e882f8671d
Tabea Schneider <abeat@posteo.net>
2025-04-08 15:45:29 -0400

    add question section to response

--- src/main.rs
+++ src/main.rs
@@ -2,65 +2,8 @@
 use std::net::UdpSocket;
 use bytes::{ BufMut, BytesMut };
 
-pub struct DNSHeader {
-    id: u16,
-    flags: DNSFlags,
-    qd_count: u16,
-    an_count: u16,
-    ns_count: u16,
-    ar_count: u16,
-}
-
-impl DNSHeader {
-    pub fn to_bytes(&self) -> BytesMut {
-        let mut buf = BytesMut::with_capacity(12);
-
-        buf.put_u16(self.id);
-        buf.put_u16(self.flags.construct_flag_bytes());
-        buf.put_u16(self.qd_count);
-        buf.put_u16(self.an_count);
-        buf.put_u16(self.ns_count);
-        buf.put_u16(self.ar_count);
-
-        buf
-    }
-}
-
-struct DNSFlags {
-    qr: bool,
-    opcode: u8, //will become 4 bits later, so max is 0xF
-    aa: bool,
-    tc: bool,
-    rd: bool,
-    ra: bool,
-    z: u8, //will become 3 bits later, so max is 0x7
-    rcode: u8, //will become 4 bits later, so max is 0xF
-}
-
-impl DNSFlags {
-    fn new_qr_only() -> Self {
-        Self {
-            qr: true,
-            opcode: 0,
-            aa: false,
-            tc: false,
-            rd: false,
-            ra: false,
-            z: 0,
-            rcode: 0,
-        }
-    }
-    fn construct_flag_bytes(&self) -> u16 {
-        ((self.qr as u16) << 15) |
-            (((self.opcode as u16) & 0xf) << 11) | // 11-14
-            ((self.aa as u16) << 10) |
-            ((self.tc as u16) << 9) |
-            ((self.rd as u16) << 8) |
-            ((self.ra as u16) << 7) |
-            (((self.z as u16) & 0x7) << 4) | //  4-6
-            ((self.rcode as u16) & 0xf) // 0-4
-    }
-}
+use codecrafters_dns_server::{ DNSHeader, DNSFlags };
+use codecrafters_dns_server::DNSQuestion;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -74,22 +17,13 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-
-                let flags: DNSFlags = DNSFlags::new_qr_only();
-
-                let num: u16 = 1234;
-
-                let header = DNSHeader {
-                    id: num,
-                    flags,
-                    qd_count: 0,
-                    an_count: 0,
-                    ns_count: 0,
-                    ar_count: 0,
-                };
-
-                let response = header.to_bytes();
-
+                let labels = vec!["codecrafters".to_string(), "io".to_string()];
+                let flags: DNSFlags = DNSFlags::new(true, 0, false, false, false, false, 0, 0);
+                let header = DNSHeader::new(1234, flags, 1, 0, 0, 0);
+                let question = DNSQuestion::new_atype_inclass(labels);
+                let mut response = BytesMut::new();
+                response.put(header.to_bytes());
+                response.put(question.to_bytes());
                 udp_socket.send_to(response.as_ref(), source).expect("Failed to send response");
             }
             Err(e) => {
