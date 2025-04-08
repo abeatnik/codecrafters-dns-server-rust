dc8dc42acda2a841a2b4774404bee6a03b8f3725
Tabea Schneider <abeat@posteo.net>
2025-04-08 12:32:32 -0400

    send header

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
 #[allow(unused_imports)]
 use std::net::UdpSocket;
+use bytes::{ BufMut, BytesMut };
+
+pub struct DNSHeader {
+    id: u16,
+    flags: u16,
+    qd_count: u16,
+    an_count: u16,
+    ns_count: u16,
+    ar_count: u16,
+}
+
+impl DNSHeader {
+    pub fn to_bytes(&self) -> BytesMut {
+        let mut buf = BytesMut::with_capacity(12);
+
+        buf.put_u16(self.id);
+        buf.put_u16(self.flags);
+        buf.put_u16(self.qd_count);
+        buf.put_u16(self.an_count);
+        buf.put_u16(self.ns_count);
+        buf.put_u16(self.ar_count);
+
+        buf
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,8 +38,25 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
-                udp_socket.send_to(&response, source).expect("Failed to send response");
+
+                let flags: u16 = 0b1000_0000_0000_0000;
+
+                let num: u16 = 1234;
+
+                let header = DNSHeader {
+                    id: num,
+                    flags,
+                    qd_count: 0,
+                    an_count: 0,
+                    ns_count: 0,
+                    ar_count: 0,
+                };
+
+                let response = header.to_bytes();
+
+                println!("{:?}", &response);
+
+                udp_socket.send_to(response.as_ref(), source).expect("Failed to send response");
             }
             Err(e) => {
                 eprintln!("Error receiving data: {}", e);
